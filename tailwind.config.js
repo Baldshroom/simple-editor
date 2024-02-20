c8dc2454d2d3873b5ec3340ea7427bbcefd2dbe5
Oskar <o.pulczynski@gmail.com>
2024-02-20 21:06:05 +0100

    Added fonts to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
 	content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				wtf: "hotpink",
+			},
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
 	},
 	plugins: [],
 };
